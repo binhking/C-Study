4db47f1acd0eebe8fa04d774bb640c3389752c0e
binhking <kjournal@163.com>
2022-10-01 13:54:56 +0800

    add file

---
+++ 二分查找.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int key = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int left_index = 0;
+    int right_index = size - 1;
+
+    while (left_index <= right_index)
+    {
+        int mid = (left_index + right_index) / 2;
+        if (arr[mid] > key)
+        {
+            right_index = mid - 1;
+        }
+        else if (arr[mid] < key)
+        {
+            left_index = mid + 1;
+        }
+        else
+        {
+            printf("找到了 下标是：%d\n", mid);
+            break;
+        }
+    }
+    if (left_index > right_index)
+    {
+        printf("没有找到\n");
+    }
+
+    return 0;
+}
