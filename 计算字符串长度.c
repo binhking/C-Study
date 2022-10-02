a87cb7b2f99eeb328f44eb4cd5b97161a37b61f1
binhking <kjournal@163.com>
2022-10-02 16:02:33 +0800

    add file

---
+++ 计算字符串长度.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int str_len(char *str)
+{
+
+    if (*str != '\0')
+
+        return 1 + str_len(str + 1);
+
+    else
+        return 0;
+}
+
+int main()
+{
+    char arr[] = "bit";
+    int len = str_len(arr);
+    printf("%s ×Ö·û³¤¶È£º%d\n", arr, len);
+    return 0;
+}
